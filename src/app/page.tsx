cf2865d48c47790c6a44b7d0d7107c5f28723309
Veidz <joaovictorveidz@gmail.com>
2023-06-04 02:30:16 -0300

    feat: add value to input and disable button after send data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -64,15 +64,16 @@ export default function Home() {
         setName('')
         setImageUrl('')
         setItemUrl('')
+        setButtonDisabled(true)
     }
 
     return (
         <main className='flex min-h-screen flex-col items-center p-24'>
             <h1 className='text-8xl font-bold mb-10 tracking-wide'>WishList</h1>
             <form>
-                <input onChange={ (e) => handleNameChange(e) } className='p-4 m-4 outline-blue-500' type='text' placeholder='Item name' required />
-                <input onChange={ (e) => handleImageUrlChange(e) } className='p-4 m-4 outline-blue-500' type='url' placeholder='Image url' />
-                <input onChange={ (e) => handleItemUrlChange(e) } className='p-4 m-4 outline-blue-500' type='url' placeholder='Item url' required />
+                <input onChange={ (e) => handleNameChange(e) } className='p-4 m-4 outline-blue-500' type='text' placeholder='Item name' required value={ name } />
+                <input onChange={ (e) => handleImageUrlChange(e) } className='p-4 m-4 outline-blue-500' type='url' placeholder='Image url' value={ imageUrl } />
+                <input onChange={ (e) => handleItemUrlChange(e) } className='p-4 m-4 outline-blue-500' type='url' placeholder='Item url' required value={ itemUrl } />
                 <button className='bg-blue-500 hover:enabled:bg-blue-700 text-white font-bold p-4 m-4 rounded w-40 disabled:cursor-not-allowed disabled:opacity-50' onClick={ (e) => addItem(e) } type='submit' disabled={ buttonDisabled }>Add</button>
                 
                 <ToastContainer />
