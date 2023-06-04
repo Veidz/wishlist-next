33113420629bc99183a4a676928a066902fa1006
Veidz <joaovictorveidz@gmail.com>
2023-06-04 02:26:21 -0300

    style: add outline color to input

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -71,8 +71,8 @@ export default function Home() {
             <h1 className='text-8xl font-bold mb-10 tracking-wide'>WishList</h1>
             <form>
                 <input onChange={ (e) => handleNameChange(e) } className='p-4 m-4 outline-blue-500' type='text' placeholder='Item name' required />
-                <input onChange={ (e) => handleImageUrlChange(e) } className='p-4 m-4' type='url' placeholder='Image url' />
-                <input onChange={ (e) => handleItemUrlChange(e) } className='p-4 m-4' type='url' placeholder='Item url' required />
+                <input onChange={ (e) => handleImageUrlChange(e) } className='p-4 m-4 outline-blue-500' type='url' placeholder='Image url' />
+                <input onChange={ (e) => handleItemUrlChange(e) } className='p-4 m-4 outline-blue-500' type='url' placeholder='Item url' required />
                 <button className='bg-blue-500 hover:enabled:bg-blue-700 text-white font-bold p-4 m-4 rounded w-40 disabled:cursor-not-allowed disabled:opacity-50' onClick={ (e) => addItem(e) } type='submit' disabled={ buttonDisabled }>Add</button>
                 
                 <ToastContainer />
