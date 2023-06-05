9ff19fb2c69748723c1d932207e000e36522494b
Veidz <joaovictorveidz@gmail.com>
2023-06-04 21:52:42 -0300

    feat: create card container to group product details

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 'use client'
 
 import axios from 'axios'
@@ -98,14 +99,25 @@ export default function Home() {
                 <button className='bg-blue-500 hover:enabled:bg-blue-700 text-white font-bold p-4 m-4 rounded w-40 disabled:cursor-not-allowed disabled:opacity-50' onClick={ (e) => addProduct(e) } type='submit' disabled={ buttonDisabled }>Add</button>
             </form>
 
-            <div className=''>
+            <div className='grid grid-cols-4 gap-4'>
                 {
                     products.map((product: any) => {
                         return (
-                            <div key={ product }>
-                                <h1 className='text-2xl'>{ product.name }</h1>
-                                <Image src={ product.imageUrl } alt={`${product.name} photo`} />
-                                <a href={ product.productUrl } target="_blank">Go to Product</a>
+                            <div key={ product.name } className="flex-col mt-10 p-6 bg-white border border-gray-200 rounded-lg flex items-center justify-between">
+                                <img src={ product.imageUrl } alt={ `${product.name} photo` } width={ 64 } height={ 64 } />
+                                
+                                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">
+                                    { product.name }
+                                </h5>
+
+                                <a href={ product.productUrl } target='_blank' className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                    Go to product
+
+                                    <svg aria-hidden="true" className="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                        <path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd">
+                                        </path>
+                                    </svg>
+                                </a>
                             </div>
                         )
                     })
